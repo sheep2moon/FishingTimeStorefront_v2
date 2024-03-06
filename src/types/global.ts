ff7022ed0d52112d27ba791d93ae6951d69049f5
Konrad Iwan <kon.iwan17@gmail.com>
2024-03-06 18:38:29 +0100

    add medusa api calls from storefront template

---
+++ src/types/global.ts
@@ -0,0 +1,21 @@
+import { ProductCategory } from "@medusajs/medusa";
+
+export type ProductPreviewType = {
+    id: string;
+    title: string;
+    handle: string | null;
+    thumbnail: string | null;
+    created_at?: Date;
+    price?: {
+        calculated_price: string;
+        original_price: string;
+        difference: string;
+        price_type: "default" | "sale";
+    };
+    isFeatured?: boolean;
+};
+
+export type ProductCategoryWithChildren = Omit<ProductCategory, "category_children"> & {
+    category_children: ProductCategory[];
+    category_parent?: ProductCategory;
+};
